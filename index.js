ba4c6639910acd018bfc7649f84767262e56f3d3
Michele Mazzucco <hello@michelemazzucco.it>
2016-05-18 21:37:47 +0200

    refactoring of indentation before lint

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-border-shortcut', function (opts) {
-    opts = opts || {};
+  opts = opts || {};
 
-    return function (css) {
+  return function (css) {
+    css.walkDecls( function (decl) {
+      if (decl.prop.match(/^(?!.*(style|width|color)).*border.*$/)) {
+        var valueList = postcss.list.space(decl.value);
+        var prop = decl.prop;
+        var FirstValue = valueList[0];
+        var LastValue = valueList[2];
 
-        css.walkDecls( function (decl) {
-            if (decl.prop.match(/^(?!.*(style|width|color)).*border.*$/)) {
-                var valueList = postcss.list.space(decl.value);
-                var prop = decl.prop;
-                var FirstValue = valueList[0];
-                var LastValue = valueList[2];
-
-            if (valueList.length <= 2) {
-              LastValue = valueList[1];
-              decl.replaceWith(prop + ':' + FirstValue + ' solid ' + LastValue);
-            }
-          }
-        });
-    };
+        if (valueList.length <= 2) {
+          LastValue = valueList[1];
+          decl.replaceWith(prop + ':' + FirstValue + ' solid ' + LastValue);
+        }
+      }
+    });
+  };
 });
