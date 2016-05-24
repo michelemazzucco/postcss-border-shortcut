278a7475078e404e7078de32ba39f006b1fdefd4
Michele Mazzucco <hello@michelemazzucco.it>
2016-05-24 13:58:42 +0200

    wip on opts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-border-shortcut', function (opts) {
   opts = opts || {};
-
+  var type = opts.type || 'solid';
   return function (css) {
     css.walkDecls( function (decl) {
       var borderRegExp = /^(?!.*(style|width|color|radius)).*border.*$/,
@@ -16,10 +16,10 @@ module.exports = postcss.plugin('postcss-border-shortcut', function (opts) {
 
         if (valueList.length === 1 && firstValue.match(unitRegExp) && firstValue.length >= 2) {
           lastValue = firstValue;
-          decl.replaceWith(prop + ': 1px solid ' + lastValue);
+          decl.replaceWith(prop + ': 1px ' + type + lastValue);
         } else if (valueList.length === 2) {
           lastValue = valueList[1];
-          decl.replaceWith(prop + ':' + firstValue + ' solid ' + lastValue);
+          decl.replaceWith(prop + ':' + firstValue + type + lastValue);
         }
       }
     });
