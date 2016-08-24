c6d29c393fe2e10fbb199b551d1c47d48e36beea
Michele Mazzucco <hello@michelemazzucco.it>
2016-08-24 16:18:27 +0200

    refaction wip, 0.1.0 is coming

--- index.js
+++ index.js
@@ -9,25 +9,34 @@ module.exports = postcss.plugin('postcss-border-shortcut', function (opts) {
   opts = opts || {};
   var type = opts.borderType || 'solid';
 
-  function parseBorder(el) {
-    var value_list = postcss.list.space(el.value);
-    var prop = el.prop + ': ';
+  function parseBorder(v, l) {
     var result = '';
+    var value_list = l;
 
-    if (value_list[0].length >= 2){
-      if (value_list.length === 1 && value_list[0].match(regex.color)) {
-        result = '1px ' + type + ' ' + value_list[0];
-        el.replaceWith(prop + result)
-      } else if (value_list.length === 2) {
-        result = value_list[0] + ' ' + type + ' ' + value_list[1];
-        el.replaceWith(prop + result)
-      }
+    if (value_list.length === 1 && value_list[0].match(regex.color)) {
+      result = '1px ' + type + ' ' + value_list[0];
+    } else if (value_list.length === 2) {
+      result = value_list[0] + ' ' + type + ' ' + value_list[1];
+    } else {
+      result = v;
+    }
+
+    return result;
+  }
+
+  function isBorder(d) {
+    var value = d.value;
+    var value_prop = d.prop + ': ';
+    var value_list = postcss.list.space(value);
+
+    if (value_prop.match(regex.type) && value_list[0].length >= 2) {
+      d.replaceWith(prop + parseBorder(value, value_list));
     }
   }
 
   return function (css) {
-    css.walkDecls(regex.type, function(decl) {
-      parseBorder(decl)
+    css.walkDecls(regex.type, function (decl) {
+      isBorder(decl);
     });
   };
 });
